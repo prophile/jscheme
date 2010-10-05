124bcd042174dff98c977e94421e2cb90b4f8fe3
Alastair Lynn <arplynn@gmail.com>
2010-10-05 20:08:52 +0100

    Added a Scope implementation.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

---
+++ Scope.java
@@ -0,0 +1,35 @@
+package com.hovercatsw.alistair.jscheme;
+
+import java.util.HashMap;
+
+public class Scope
+{
+	private Scope enclosingScope;
+	private HashMap<String, Value> variables = new HashMap<String, Value>();
+
+	public Scope()
+	{
+		enclosingScope = null;
+	}
+
+	public Scope(Scope enclosing)
+	{
+		enclosingScope = enclosing;
+	}
+
+	public void put(String key, Value val)
+	{
+		variables.put(key, val);
+	}
+
+	public Value lookup(String key)
+	{
+		Value rv;
+		if ((rv = variables.get(key)) != null)
+			return rv;
+		else if (enclosingScope != null)
+			return enclosingScope.lookup(key);
+		else
+			return null;
+	}
+}
